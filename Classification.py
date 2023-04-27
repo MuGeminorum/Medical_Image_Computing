32f838e5c0908dad8b37345153d2514229fac5a6
MuGeminorum <20459298+MuGeminorum@users.noreply.github.com>
2023-04-27 21:17:32 +0800

    add requirements

--- Classification.py
+++ Classification.py
@@ -1,35 +1,32 @@
-import os
-from csv import reader
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import svm
+from datasets import load_dataset
 from sklearn.svm import LinearSVC
+
 
 def main():
     # Load data
-    with open("data/pima.csv") as f:
-        csv_data = reader(f, delimiter=',')
-        raw_data = np.array(list(csv_data))
+    data = load_dataset("george-chou/Pima")
+    trainset = data['train']
+    testset = data['test']
 
     # Preprocess data
-    data_x = []
-    data_y = []
-    tuple_len = len(raw_data[0])
-    for i in raw_data:
-        if not i:
-            continue
-        data_x.append([float(j) for j in i[0:tuple_len - 2]])
-        if i[tuple_len - 1] == "yes":
-            data_y.append(1)
-        else:
-            data_y.append(0)
+    x_train, y_train, x_test, y_test = [], [], [], []
 
-    # Split dataset
-    x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.33, random_state=73)
+    for item in trainset:
+        item_vals = list(item.values())
+        x_train.append(item_vals[1:-2])
+        y_train.append(item_vals[-1])
 
-    #TODO:
+    for item in testset:
+        item_vals = list(item.values())
+        x_test.append(item_vals[1:-2])
+        y_test.append(item_vals[-1])
+
+    # Train
     clf = LinearSVC(loss="hinge", random_state=42).fit(x_train, y_train)
+
+    # Test
     print(clf.score(x_test, y_test))
+
 
 if __name__ == "__main__":
     main()
